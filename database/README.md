c24554e81cfa89f419ca41d2a61449dcdbbfc37b
otavio.lima <otavio.lima@itaipuparquetec.org.br>
2025-05-28 09:35:09 -0300

    add REAMD.md + alteração no esquema da base de dados

---
+++ database/README.md
@@ -0,0 +1,16 @@
+| Código           | Descrição                             |
+| ---------------- | ------------------------------------- |
+| `class`          | Aula (presencial ou online)           |
+| `exam`           | Prova ou avaliação escrita            |
+| `quiz`           | Teste rápido                          |
+| `assignment`     | Entrega de atividade                  |
+| `lecture`        | Palestra                              |
+| `workshop`       | Oficina prática                       |
+| `seminar`        | Seminário                             |
+| `presentation`   | Apresentação de trabalho              |
+| `deadline`       | Prazo final (para qualquer entrega)   |
+| `holiday`        | Feriado                               |
+| `announcement`   | Aviso ou comunicado importante        |
+| `cultural`       | Evento cultural (ex: feira literária) |
+| `sports`         | Evento esportivo                      |
+| `other`          | Outro tipo não especificado           |
--- database/schema.sql
+++ database/schema.sql
@@ -1,151 +1,178 @@
 USE `intellecta`;
 
+CREATE TABLE `files` (
+  `file_id` CHAR(36) PRIMARY KEY,
+  `url` TEXT NOT NULL,
+  `filename` VARCHAR(255) NOT NULL,
+  `mime_type` VARCHAR(100),
+  `size` INT UNSIGNED, -- Tamanho em bytes
+  `uploaded_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Tabela de usuário
 CREATE TABLE `users` (
-  `id_user` CHAR(36) PRIMARY KEY,
+  `user_id` CHAR(36) PRIMARY KEY,
   `full_name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) UNIQUE NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `status` ENUM ('active', 'deactivated') NOT NULL DEFAULT 'deactivated', -- 'active' significa que o usuário verificou o e-mail que utilizou ao criar a conta e 'deactivated' o contrário.
+  `password_hash` VARCHAR(255) NOT NULL,
+  `email_verified` BOOL NOT NULL DEFAULT false, -- 'true' significa que o usuário verificou o e-mail que utilizou ao criar a conta e 'false' o contrário.
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `profile_picture_url` VARCHAR(512) -- URL da imagem de perfil do usuário
+  `profile_picture_id` CHAR(36),
+  FOREIGN KEY (`profile_picture_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL
 );
 
 -- Tabela de instituição
 CREATE TABLE `institutions` (
-  `id_institution` CHAR(36) PRIMARY KEY,
+  `institution_id` CHAR(36) PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `phone_number` VARCHAR(255),
   `description` TEXT,
-  `thumbnail_url` VARCHAR(512), -- URL da imagem de perfil da instituição
-  `banner_url` VARCHAR(512), -- URL do banner da instituição
-  `id_user` CHAR(36) NOT NULL, -- ID do usuário que criou a instituição
-  FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+  `thumbnail_id` CHAR(36),
+  `banner_id` CHAR(36),
+  `user_id` CHAR(36) NOT NULL, -- ID do usuário que criou a instituição
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`thumbnail_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`banner_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL
 );
 
--- Tabela de relação MxN entre User e Institution
+-- Tabela de relação MxN entre users e institutions
 CREATE TABLE `institution_users` (
-  `id_institute_user` CHAR(36) PRIMARY KEY,
-  `id_institution` CHAR(36) NOT NULL,
-  `id_user` CHAR(36) NOT NULL,
-  `role` ENUM ('student', 'teacher', 'admin') NOT NULL DEFAULT 'student',
+  `institution_users_id` CHAR(36) PRIMARY KEY,
+  `role` ENUM('student', 'teacher', 'admin') NOT NULL DEFAULT 'student',
   `joined_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`id_institution`) REFERENCES `institutions`(`id_institution`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE,
-  UNIQUE (`id_institution`, `id_user`)
+  `institution_id` CHAR(36) NOT NULL,
+  `owner_id` CHAR(36) NOT NULL,
+  FOREIGN KEY (`institution_id`) REFERENCES `institutions`(`institution_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`owner_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE,
+  UNIQUE (`institution_id`, `user_id`)
 );
 
 -- Tabela que armazena os convites de participação de uma instituição
 CREATE TABLE `invitations` (
-  `id_invitation` CHAR(36) NOT NULL PRIMARY KEY,
+  `invitation_id` CHAR(36) NOT NULL PRIMARY KEY,
   `email` VARCHAR(255) NOT NULL,
-  `role` ENUM ('student', 'teacher', 'admin') NOT NULL DEFAULT 'student',
+  `role` ENUM('student', 'teacher', 'admin') NOT NULL DEFAULT 'student',
   `token` CHAR(64) NOT NULL UNIQUE,
-  `expires_at` DATETIME NOT NULL,
-  `accepted_at` DATETIME DEFAULT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id_institution` CHAR(36) NOT NULL,
-  -- Relação com a instituição (opcionalmente com FOREIGN KEY)
-  FOREIGN KEY (`id_institution`) REFERENCES `institutions`(`id_institution`) ON DELETE CASCADE
+  `expires_at` TIMESTAMP NOT NULL,
+  `accepted_at` TIMESTAMP DEFAULT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `institution_id` CHAR(36) NOT NULL,
+  `invited_by` CHAR(36) NOT NULL,
+  FOREIGN KEY (`invited_by`) REFERENCES `users`(`user_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`institution_id`) REFERENCES `institutions`(`institution_id`) ON DELETE CASCADE
 );
 
 -- Tabela de turma
 CREATE TABLE `classes` (
-  `id_class` CHAR(36) PRIMARY KEY,
+  `class_id` CHAR(36) PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
-  `thumbnail_url` VARCHAR(512), -- URL da imagem de perfil da turma
-  `banner_url` VARCHAR(512), -- URL do banner da turma
-  `id_institution` CHAR(36) NOT NULL, -- ID da instituição à qual a turma pertence
-  FOREIGN KEY (`id_institution`) REFERENCES `institutions`(`id_institution`) ON DELETE CASCADE
+  `thumbnail_id` CHAR(36),
+  `banner_id` CHAR(36),
+  `institution_id` CHAR(36) NOT NULL, -- ID da instituição à qual a turma pertence
+  FOREIGN KEY (`institution_id`) REFERENCES `institutions`(`institution_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`thumbnail_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`banner_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL
 );
 
 -- Tabela de relação entre usuário e turma
 CREATE TABLE `class_users` (
-  `id_class_user` CHAR(36) PRIMARY KEY,
-  `id_class` CHAR(36) NOT NULL,
-  `id_user` CHAR(36) NOT NULL, -- ID do usuário participante da turma -- aluno.
+  `class_users_id` CHAR(36) PRIMARY KEY,
   `joined_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`id_class`) REFERENCES `classes`(`id_class`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+  `class_id` CHAR(36) NOT NULL,
+  `user_id` CHAR(36) NOT NULL, -- ID do usuário participante da turma -- aluno.
+  FOREIGN KEY (`class_id`) REFERENCES `classes`(`class_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
 );
 
 -- Tabela de Disciplina
 CREATE TABLE `subjects` (
-  `id_subject` CHAR(36) PRIMARY KEY,
+  `subject_id` CHAR(36) PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `description` TEXT,
-  `thumbnail_url` VARCHAR(512), -- URL da imagem de perfil da disciplina
-  `banner_url` VARCHAR(512), -- URL do banner da disciplina
-  `id_institution` CHAR(36) NOT NULL, -- ID da instituição à qual a disciplina pertence
-  `id_user` CHAR(36) NOT NULL, -- ID do usuário que criou a disciplina -- Professor da disciplina.
-  FOREIGN KEY (`id_institution`) REFERENCES `institutions`(`id_institution`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+  `thumbnail_id` CHAR(36),
+  `banner_id` CHAR(36),
+  `institution_id` CHAR(36) NOT NULL, -- ID da instituição à qual a disciplina pertence
+  `professor_id` CHAR(36), -- ID do usuário que criou a disciplina -- Professor da disciplina.
+  FOREIGN KEY (`institution_id`) REFERENCES `institutions`(`institution_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`professor_id`) REFERENCES `users`(`user_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`thumbnail_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`banner_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL
 );
 
 -- Tabela de relação entre turma e disciplina
 CREATE TABLE `subject_classes` (
-  `id_subject_class` CHAR(36) PRIMARY KEY,
-  `id_class` CHAR(36) NOT NULL,
-  `id_subject` CHAR(36) NOT NULL,
-  FOREIGN KEY (`id_class`) REFERENCES `classes`(`id_class`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_subject`) REFERENCES `subjects`(`id_subject`) ON DELETE CASCADE
+  `subject_classes_id` CHAR(36) PRIMARY KEY,
+  `class_id` CHAR(36) NOT NULL,
+  `subject_id` CHAR(36) NOT NULL,
+  FOREIGN KEY (`class_id`) REFERENCES `classes`(`class_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`subject_id`) REFERENCES `subjects`(`subject_id`) ON DELETE CASCADE,
+  UNIQUE(`subject_id`, `class_id`)
 );
 
 -- Tabela de material da disciplina
 CREATE TABLE `materials` (
-  `id_material` CHAR(36) PRIMARY KEY,
+  `material_id` CHAR(36) PRIMARY KEY,
   `title` VARCHAR(255) NOT NULL,
-  `file_url` TEXT NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id_subject` CHAR(36) NOT NULL,
-  FOREIGN KEY (`id_subject`) REFERENCES `subjects`(`id_subject`) ON DELETE CASCADE
+  `subject_id` CHAR(36) NOT NULL,
+  `file_id` CHAR(36) NOT NULL,
+  FOREIGN KEY (`file_id`) REFERENCES `files`(`file_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`subject_id`) REFERENCES `subjects`(`subject_id`) ON DELETE CASCADE
 );
 
 -- Tabela de atividade avaliativa
 CREATE TABLE `assignments` (
-  `id_assignment` CHAR(36) PRIMARY KEY,
+  `assignment_id` CHAR(36) PRIMARY KEY,
   `title` VARCHAR(255) NOT NULL,
   `description` TEXT NOT NULL,
   `deadline` DATE NOT NULL,
-  `attachment` TEXT,
-  `id_subject` CHAR(36) NOT NULL,
-  FOREIGN KEY (`id_subject`) REFERENCES `subjects`(`id_subject`) ON DELETE CASCADE
+  `subject_id` CHAR(36) NOT NULL,
+  `attachment_id` CHAR(36),
+  FOREIGN KEY (`attachment_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`subject_id`) REFERENCES `subjects`(`subject_id`) ON DELETE CASCADE
 );
 
 -- Tabela de entrega de atividade avaliativa
 CREATE TABLE `submissions` (
-  `id_submission` CHAR(36) PRIMARY KEY,
-  `id_assignment` CHAR(36) NOT NULL,
-  `id_user` CHAR(36) NOT NULL,
+  `submission_id` CHAR(36) PRIMARY KEY,
   `submitted_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `file` TEXT,
   `grade` DECIMAL,
   `concept` VARCHAR(255),
   `feedback` TEXT,
-  FOREIGN KEY (`id_assignment`) REFERENCES `assignments`(`id_assignment`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+  `assignment_id` CHAR(36) NOT NULL,
+  `user_id` CHAR(36) NOT NULL,
+  `file_id` CHAR(36),
+  FOREIGN KEY (`file_id`) REFERENCES `files`(`file_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`assignment_id`) REFERENCES `assignments`(`assignment_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE SET NULL
 );
 
 -- Tabela de mensgens do fórum de disciplina
 CREATE TABLE `forum_messages` (
-  `id_message` CHAR(36) PRIMARY KEY,
-  `id_subject` CHAR(36) NOT NULL,
+  `forum_messages_id` CHAR(36) PRIMARY KEY,
   `content` TEXT NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (`id_subject`) REFERENCES `subjects`(`id_subject`) ON DELETE CASCADE
+  `sent_by` CHAR(36),
+  `subject_id` CHAR(36) NOT NULL,
+  FOREIGN KEY (`sent_by`) REFERENCES `users`(`user_id`) ON DELETE SET NULL,
+  FOREIGN KEY (`subject_id`) REFERENCES `subjects`(`subject_id`) ON DELETE CASCADE
 );
 
 -- Tabela 'pai' dos eventos. As outras 'herdam' dessa via padrão de composição.
 CREATE TABLE `events` (
-  `id_event` CHAR(36) PRIMARY KEY,
+  `event_id` CHAR(36) PRIMARY KEY,
   `title` VARCHAR(255) NOT NULL,
   `description` TEXT,
+  `type` ENUM(
+    'class', 'exam', 'quiz', 'assignment', 'lecture', 'workshop',
+    'seminar', 'presentation', 'deadline',
+    'holiday', 'announcement', 'cultural', 'sports', 'other'
+  ) DEFAULT 'other', -- Tipos de evento, mais detalhes podem ser encontados em ./README.md
   `event_date` TIMESTAMP NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `changed_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -153,34 +180,34 @@ CREATE TABLE `events` (
 
 -- Eventos globais da instituição
 CREATE TABLE `institutional_events` (
-  `id_institutional_event` CHAR(36) PRIMARY KEY,
-  `id_event` CHAR(36) NOT NULL,
-  `id_institution` CHAR(36) NOT NULL,
-  FOREIGN KEY (`id_event`) REFERENCES `events`(`id_event`) ON DELETE CASCADE, 
-  FOREIGN KEY (`id_institution`) REFERENCES `institutions`(`id_institution`) ON DELETE CASCADE
+  `institutional_events_id` CHAR(36) PRIMARY KEY,
+  `event_id` CHAR(36) NOT NULL,
+  `institution_id` CHAR(36) NOT NULL,
+  FOREIGN KEY (`event_id`) REFERENCES `events`(`event_id`) ON DELETE CASCADE, 
+  FOREIGN KEY (`institution_id`) REFERENCES `institutions`(`institution_id`) ON DELETE CASCADE
 );
 
 -- Eventos específicos de disciplina
 CREATE TABLE `subject_events` (
-  `id_subject_event` CHAR(36) PRIMARY KEY,
-  `id_event` CHAR(36) NOT NULL,
-  `id_subject` CHAR(36) NOT NULL,
-  FOREIGN KEY (`id_event`) REFERENCES `events`(`id_event`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_subject`) REFERENCES `subjects`(`id_subject`) ON DELETE CASCADE
+  `subject_events_id` CHAR(36) PRIMARY KEY,
+  `event_id` CHAR(36) NOT NULL,
+  `subject_id` CHAR(36) NOT NULL,
+  FOREIGN KEY (`event_id`) REFERENCES `events`(`event_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`subject_id`) REFERENCES `subjects`(`subject_id`) ON DELETE CASCADE
 );
 
 -- Eventos específicos de usuário
 CREATE TABLE `user_events` (
-  `id_user_event` CHAR(36) PRIMARY KEY,
-  `id_event` CHAR(36) NOT NULL,
-  `id_user` CHAR(36) NOT NULL,
-  FOREIGN KEY (`id_event`) REFERENCES `events`(`id_event`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_user`) REFERENCES `users`(`id_user`) ON DELETE CASCADE
+  `user_events_id` CHAR(36) PRIMARY KEY,
+  `event_id` CHAR(36) NOT NULL,
+  `user_id` CHAR(36) NOT NULL,
+  FOREIGN KEY (`event_id`) REFERENCES `events`(`event_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
 );
 
 -- Tabela de tokens JWT invalidados
 CREATE TABLE `jwt_blacklists` (
-  `id_jwt_blacklists` CHAR(36) NOT NULL PRIMARY KEY,
+  `jwt_blacklists_id` CHAR(36) NOT NULL PRIMARY KEY,
   `token` TEXT,
   `expires_at` DATETIME NOT NULL
 );
